e490ce23671c8cadf4376d064099eb17626da5e7
Hyperion <ehsonmahmadmurodov@gmail.com>
2026-07-10 20:51:05 +0500

    Add generated app screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,10 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SocialHome',
+      title: 'SocialHomeDemo',
       home: const MyHomePage(),
     );
   }
@@ -16,25 +15,54 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('SocialHome'),
+        title: const Text('SocialHomeDemo'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              child: const Text('Feed'),
-              onPressed: () => ScaffoldMessenger.of(context)
-                  .showSnackBar(const SnackBar(content: Text('Feed'))),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Create Post')));
+              },
+              child: const Text('Create Post'),
             ),
             const SizedBox(height: 12),
             ElevatedButton(
-              child: const Text(' BAC Messages'),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('View Feed')));
+              },
+              child: const Text('View Feed'),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Messages')));
+              },
+              child: const Text('Messages'),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Profile')));
+              },
+              child: const Text('Profile'),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Settings')));
+              },
+              child: const Text('Settings'),
             ),
           ],
         ),
